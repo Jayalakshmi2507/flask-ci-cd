f807061205d5f5f65bace0973bc7dbfc2b2e2518
jayalakshmikrishnan25@gmail.com <jayalakshmikrishnan25@gmail.com>
2025-05-09 15:31:04 +0530

    Adding docker.tf file

---
+++ docker.tf
@@ -0,0 +1,18 @@
+provider "docker" {}
+
+resource "docker_image" "flask_app" {
+  name         = "flask-app"
+  build {
+    context    = "."
+    dockerfile = "Dockerfile"
+  }
+}
+
+resource "docker_container" "flask_container" {
+  name  = "flask-container"
+  image = docker_image.flask_app.image_id
+  ports {
+    internal = 5000
+    external = 5000
+  }
+}
